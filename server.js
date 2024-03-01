cdd115fe490b8a469fab2a1bcd12ea3a97164968
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-02-29 19:17:13 -0600

    Connected to mongoDB with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const connectionString = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
+mongoose.connect(connectionString, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}).then(con=>{console.log(con.connections), console.log(
+  "DB connection successful"
+)});
 
 ////////////////////////////////////////////////////////////////
 // starting the server
