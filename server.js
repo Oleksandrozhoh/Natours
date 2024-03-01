10fec869b073c3f3c99efb4f0004e25cd7cb0140
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-02-29 20:19:16 -0600

    Created mongoose schema, model and saved tour document in tours collection

--- server.js
+++ server.js
@@ -4,10 +4,34 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+// connection to MongoDB
 const connectionString = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
-mongoose.connect(connectionString, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}).then(con=>{console.log(con.connections), console.log(
-  "DB connection successful"
-)});
+mongoose.connect(connectionString, {
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useFindAndModify: false
+}).then(()=>console.log("DB connection successful"));
+
+//creating schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "A tour must have a name"],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    require: [true, "A tour must have a price"]
+  }
+})
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({name: "My test tour", rating: 5.0, price:500});
+testTour.save().then(doc => {console.log(doc)}).catch(err => console.log("😒😒😒😒",err));
 
 ////////////////////////////////////////////////////////////////
 // starting the server
