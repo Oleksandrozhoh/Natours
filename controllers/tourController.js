95e20aaebea3e1cd6297c3c221484d676e52321f
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-02 15:02:11 -0600

    get tours sorting filter option

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,4 @@
-const Tour = require('../models/tourModel')
-
-
+const Tour = require('../models/tourModel');
 
 ////////////////////////////////////////
 // route middleware
@@ -8,106 +6,114 @@ const Tour = require('../models/tourModel')
 ////////////////////////////////////////
 // route handlers
 exports.getAllTours = async (req, res) => {
-  try{
+  try {
     // filtering
-    const queryObj = {...req.query};
+    const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(field => delete queryObj[field]);
+    excludedFields.forEach((field) => delete queryObj[field]);
     // advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     // build query
-    const query = await Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    // execution query
+    // sorting ( ?sort=fieldName (asc) || ?sort=-filedName (desc) )
+    if (req.query.sort) {
+      const sortByStr = JSON.stringify(req.query.sort).replace(',', ' ');
+      const sortBy = JSON.parse(sortByStr);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // executing query
     const allTours = await query;
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       results: allTours.length,
       data: {
-        allTours
-      }
-    })
-  }catch(err){
+        allTours,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
       status: 'Bad request',
-      message: err.message
-    })
+      message: err.message,
+    });
   }
-  };
-  
+};
+
 exports.getTour = async (req, res) => {
-  try{
+  try {
     const tour = await Tour.findById(req.params.id);
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: {
-        tour
-      }
-    })
-  }catch(err){
+        tour,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
       status: 'Bad request',
-      message: err.message
-    })
+      message: err.message,
+    });
   }
 };
 
 exports.createTour = async (req, res) => {
-  try{
+  try {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
       status: 'created',
-      data:{
-        tour:newTour
-      }
-    })
-  }catch(err){
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
     res.status(400).json({
       status: 'Bad request',
-      message: err.message
-    })
+      message: err.message,
+    });
   }
-
-  };
+};
 
 exports.patchTour = async (req, res) => {
-  try{
-    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
     res.status(200).json({
       status: 'Updated',
-      data: {updatedTour}
-    })
-  }catch(err){
+      data: { updatedTour },
+    });
+  } catch (err) {
     res.status(400).json({
       status: 'Bad request',
-      message: err.message
-    })
-  } 
+      message: err.message,
+    });
+  }
 };
 
 exports.deleteTour = async (req, res) => {
-  try{
-  const deletedTour = await Tour.findByIdAndDelete(req.params.id);
+  try {
+    const deletedTour = await Tour.findByIdAndDelete(req.params.id);
 
-   // Check if deletedTour is null (meaning no document was found)
-   if (!deletedTour) {
-     return res.status(404).json({
-      status: 'fail',
-      message: 'No tour found with that ID'
-    });
+    // Check if deletedTour is null (meaning no document was found)
+    if (!deletedTour) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'No tour found with that ID',
+      });
     }
 
-  res.status(204).json({
-    status: "success",
-    data: null
-  })
-  }catch(err){
-  res.status(400).json({
-    status: 'Bad request',
-    message: err.message
-  })
-}
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Bad request',
+      message: err.message,
+    });
+  }
 };
