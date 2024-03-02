16dc38fd6d7a1182d190bc956c43da6cf12a6ac2
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-01 20:21:01 -0600

    Updated route handlers for create tour document and read all tour documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,13 +8,12 @@ const Tour = require('../models/tourModel')
 
 ////////////////////////////////////////
 // route handlers
-exports.getAllTours = (req, res) => {
+exports.getAllTours = async (req, res) => {
+  const allTours = await Tour.find();
   res.status(200).json({
-    status: 'sucsess',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: { tours: tours },
-  });
+    status: "success",
+    data: allTours
+  })
 };
 
 exports.getTour = (req, res) => {
@@ -22,11 +21,23 @@ exports.getTour = (req, res) => {
   // res.status(200).json({ status: 'success', data: { tour: tours.find((tour) => tour.id === id) } });
 };
 
-exports.createTour = (req, res) => {
-  // const newTour = new Tour({});
-  // newTour.save();
+exports.createTour = async (req, res) => {
+  try{
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'created',
+      data:{
+        tour:newTour
+      }
+    })
+  }catch(err){
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    })
+  }
 
-};
+  };
 
 exports.patchTour = (req, res) => {
   // TODO code to update the tour record by id
