5c65cd0dd14e0b4554a8bb093daa6e03e47ad0f2
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-05 17:23:01 -0600

    global error handling midleware

--- app.js
+++ app.js
@@ -27,9 +27,24 @@ app.use('/api/v1/users', userRouter);
 
 // affter all defined routes req is still unhandled => catching all req past this point and returning an error
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(' You got an error madafaka! ');
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err); // any arg will be recognized by express as an error and will go straight to the err handling middleware
+});
+
+// global err handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
