0440e274e2c47b0f62f91cc063c81230a9730196
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-05 17:57:09 -0600

    custom error class and moved global error handling function into its own folder

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,25 +29,10 @@ app.use('/api/v1/users', userRouter);
 
 // affter all defined routes req is still unhandled => catching all req past this point and returning an error
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-  const err = new Error(' You got an error madafaka! ');
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err); // any arg will be recognized by express as an error and will go straight to the err handling middleware
+  next(new AppError(`Route: ${req.originalUrl} is NOT defined`, 404)); // any arg will be recognized by express as an error and will go straight to the err handling middleware
 });
 
 // global err handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
