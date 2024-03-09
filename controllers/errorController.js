5bc97c5e37a75a58b560fef0a754224fe6f04f4f
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-08 22:11:10 -0600

    validation errors handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -41,6 +41,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(errorMessage, 400);
 };
 
+const handleValidationErrors = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const errorMessage = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(errorMessage, 400);
+};
+
 /////////////////////////////////////
 // global error handling function
 module.exports = (err, req, res, next) => {
@@ -54,6 +60,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrors(error);
     sendErrorProd(res, error);
   }
 };
