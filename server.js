d158545e710fa68fe524eefc3724363df7d0785a
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-09 11:29:28 -0600

    uncaught error handling

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down the server...');
+  process.exit(1);
+});
+
 // load env variables from config file to node.js
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -24,9 +31,9 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-process.on('unhandledRejection', (err) => {
+process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! Shutting down the server...');
+  console.log('UNCAUGHT EXCEPTION! Shutting down the server...');
   server.close(() => {
     process.exit(1);
   });
