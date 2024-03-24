6f477e8eddcea233ab2b33911ed97a2e3d7294ea
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-24 13:22:21 -0500

    calc ratings average every time review updated

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,7 +33,7 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
-// review schema static method
+// review schema static method to calc ratings avg
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
   const stats = await this.aggregate([
     {
@@ -47,17 +47,37 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRatings,
-    ratingsAverage: stats[0].averageRating,
-  });
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRatings,
+      ratingsAverage: stats[0].averageRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
+/////////////////////////
+// caclulating average ratings every time review document created
 reviewSchema.post('save', function (document, next) {
   this.constructor.calcAverageRatings(this.tour);
   next();
 });
+// caclulating average ratings every time review document updated / deleted
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.doc = await this.findOne();
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function (query, next) {
+  await this.doc.constructor.calcAverageRatings(this.doc.tour); // calculating average ratings only after new doc saved to the DB => doing it in post query middleware
+  next();
+});
 
+//////////////////////////
 reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: 'user', select: 'name photo' });
   next();
