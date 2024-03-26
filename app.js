d46842518b9c0f6f199e0aa0565e7a88a995b690
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-26 17:13:10 -0500

    Setting up PUG with express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+// use pug templates, specifying location of the templates
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
+
+// serves static files from public folder to clients browser
+app.use(express.static(path.join(__dirname, '/public')));
 
 ////////////////////////////////////////////////////////////////
 // global middleware
@@ -43,9 +51,6 @@ app.use(hpp({ whitelist: ['duration', 'ratingsAverage', 'ratingsQuantity', 'maxG
 // dev logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-// serves static files from public folder to clients browser
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -55,7 +60,11 @@ app.use((req, res, next) => {
 });
 
 ////////////////////////////////////////////////////////////////
-// routes
+// routes pug
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+// routes api
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
