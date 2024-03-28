69566a863753769cc5fdd33f033d8f036733f98e
Alex_Cydeo <Oleksandrozhoh@gmail.com>
2024-03-27 20:07:16 -0500

    added static pug card template for each tour record we have in DB

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,16 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour data from collection
+  const tours = await Tour.find();
+
+  // build template
+
+  // render that template using tour data
+
+  res.status(200).render('overview', { title: 'All tours', tours });
+});
 
 exports.getTourview = (req, res) => {
   res.status(200).render('tour', { title: 'The forest hiker tour' });
